12fa615aee86456dd6015877b8857fd7d3f4fe10
sooyoung0927 <sooyoung040927@gmail.com>
2026-07-31 16:04:04 +0900

    ohgiraffer 프로젝트 생성

---
+++ src/main/java/com/ohgiraffer/OhgirafferApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OhgirafferApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OhgirafferApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ohgiraffer/OhgirafferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohgiraffer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OhgirafferApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
