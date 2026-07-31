6dfbf385e86144bc834040db6d8d3128d11abc61
sooyoung0927 <sooyoung040927@gmail.com>
2026-07-31 17:05:32 +0900

    test

--- src/main/java/com/ohgiraffer/OhgirafferApplication.java
+++ src/main/java/com/ohgiraffer/OhgirafferApplication.java
@@ -7,6 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableAsync
 @EnableScheduling
+sdjbfjkbfjsdbkfsjbdfjbjkfd
 @SpringBootApplication
 public class OhgirafferApplication {
 
