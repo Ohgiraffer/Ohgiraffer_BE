aa7b86e658207cde5a552f26c184ebf16d1cd484
sooyoung0927 <sooyoung040927@gmail.com>
2026-08-06 11:36:12 +0900

    Update R__user_dummy.sql

--- src/main/resources/db/seed/be3/R__user_dummy.sql
+++ src/main/resources/db/seed/be3/R__user_dummy.sql
@@ -9,10 +9,10 @@ DELETE FROM `users`;
 INSERT INTO `users`
 (`user_id`, `bootcamp_id`, `name`, `phone`, `email`, `role`, `profile_img`, `password`, `need_reset_pw`, `notification_on`, `join_date`, `leave_date`, `status`)
 VALUES
-    (1, 1, '고은해', '010-7482-8517', 'manager01@campflow.test', 'MANAGER', NULL, '$2a$12$.UXrD41avDmOR85e3Sm7e.IYGNSej6NVekeaEvGHalA2Cy4NDyTj6', TRUE, TRUE, '2026-01-01', NULL, 'ACTIVE'),
-    (2, 1, '김아름', '010-3340-5339', 'manager02@campflow.test', 'MANAGER', NULL, '$2a$12$.UXrD41avDmOR85e3Sm7e.IYGNSej6NVekeaEvGHalA2Cy4NDyTj6', TRUE, TRUE, '2026-01-01', NULL, 'ACTIVE'),
-    (3, 1, '유수정', '010-3287-5040', 'manager03@campflow.test', 'MANAGER', NULL, '$2a$12$.UXrD41avDmOR85e3Sm7e.IYGNSej6NVekeaEvGHalA2Cy4NDyTj6', TRUE, TRUE, '2026-01-01', NULL, 'ACTIVE'),
-    (4, 1, '이정기', '010-9830-5304', 'manager04@campflow.test', 'MANAGER', NULL, '$2a$12$.UXrD41avDmOR85e3Sm7e.IYGNSej6NVekeaEvGHalA2Cy4NDyTj6', TRUE, TRUE, '2026-01-01', NULL, 'ACTIVE'),
-    (5, 1, '최자경', '010-8019-7543', 'manager05@campflow.test', 'MANAGER', NULL, '$2a$12$.UXrD41avDmOR85e3Sm7e.IYGNSej6NVekeaEvGHalA2Cy4NDyTj6', TRUE, TRUE, '2026-01-01', NULL, 'ACTIVE');
+    (1, null, '고은해', '010-7482-8517', 'manager01@campflow.test', 'MANAGER', NULL, '$2a$12$.UXrD41avDmOR85e3Sm7e.IYGNSej6NVekeaEvGHalA2Cy4NDyTj6', TRUE, TRUE, '2026-01-01', NULL, 'ACTIVE'),
+    (2, null, '김아름', '010-3340-5339', 'manager02@campflow.test', 'MANAGER', NULL, '$2a$12$.UXrD41avDmOR85e3Sm7e.IYGNSej6NVekeaEvGHalA2Cy4NDyTj6', TRUE, TRUE, '2026-01-01', NULL, 'ACTIVE'),
+    (3, null, '유수정', '010-3287-5040', 'manager03@campflow.test', 'MANAGER', NULL, '$2a$12$.UXrD41avDmOR85e3Sm7e.IYGNSej6NVekeaEvGHalA2Cy4NDyTj6', TRUE, TRUE, '2026-01-01', NULL, 'ACTIVE'),
+    (4, null, '이정기', '010-9830-5304', 'manager04@campflow.test', 'MANAGER', NULL, '$2a$12$.UXrD41avDmOR85e3Sm7e.IYGNSej6NVekeaEvGHalA2Cy4NDyTj6', TRUE, TRUE, '2026-01-01', NULL, 'ACTIVE'),
+    (5, null, '최자경', '010-8019-7543', 'manager05@campflow.test', 'MANAGER', NULL, '$2a$12$.UXrD41avDmOR85e3Sm7e.IYGNSej6NVekeaEvGHalA2Cy4NDyTj6', TRUE, TRUE, '2026-01-01', NULL, 'ACTIVE');
 
 SET FOREIGN_KEY_CHECKS = 1;
