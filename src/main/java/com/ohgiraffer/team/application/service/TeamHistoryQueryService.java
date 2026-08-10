23f8d592c7d005c88a6bc0af86cf8d413ea6ff70
030clin <030clin@gmail.com>
2026-08-10 17:58:29 +0900

    [UPDATE] team 관련 쿼리 수정

--- src/main/java/com/ohgiraffer/team/application/service/TeamHistoryQueryService.java
+++ src/main/java/com/ohgiraffer/team/application/service/TeamHistoryQueryService.java
@@ -86,6 +86,7 @@ public class TeamHistoryQueryService
 
         List<TeamSnapshotMember> snapshotMembers =
                 teamHistoryRepository.findSnapshotMembers(
+                        teamPeriodId,
                         endAt
                 );
 
@@ -97,6 +98,7 @@ public class TeamHistoryQueryService
 
         List<TeamMemberHistory> memberHistories =
                 teamHistoryRepository.findHistoriesIntersectingPeriod(
+                        teamPeriodId,
                         startAt,
                         endAt
                 );
