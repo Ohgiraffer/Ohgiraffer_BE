c0b12cba6a43f4aaabf7e57b35fcd2189a26acfe
sooyoung0927 <sooyoung040927@gmail.com>
2026-08-19 11:15:46 +0900

    빌드 오류 해결

--- src/main/resources/db/migration/be1/V20260818_1910__drop_team_member_unique_team_user.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE team_member
-DROP INDEX UQ_TEAM_MEMBER;
-
-CREATE INDEX idx_team_member_team_user
-    ON team_member (team_id, user_id);
---
+++ src/main/resources/db/migration/be3/V20260818_1100__drop_team_member_unique_team_user.sql
@@ -0,0 +1,5 @@
+CREATE INDEX idx_team_member_team_user
+    ON team_member (team_id, user_id);
+
+ALTER TABLE team_member
+DROP INDEX UQ_TEAM_MEMBER;
